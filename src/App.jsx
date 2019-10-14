9c59ce809dd8f7d49e3969d5fa452c41af598133
SianLouisa <sianlouisa@hotmail.com>
2019-10-14 17:49:17 +0200

    moved messages state to app so it can be updated when message is sent from sendmessagecontainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import ChatDisplayContainer from "./components/chat-display/chat-display-container";
 import SendMessageContainer from "./components/send-message/send-message-container";
+import ChatDisplayMessagesList from "./components/chat-display/chat-display-messages-list";
+import { getAllMessages } from "./api/api"
 
-function App() {
-  return (
-    <div className="app">
-      <ChatDisplayContainer />
-      <SendMessageContainer />
-    </div>
-  );
+class App extends React.Component {
+  state = { previousMessages: [] }
+  
+  fetchMessages = () => {
+    getAllMessages()
+      .then(previousMessages => this.setState({ previousMessages }));
+  }
+
+  componentDidMount() {
+    this.fetchMessages();
+  }
+
+  updateMessages = () => {
+    this.fetchMessages();
+  }
+  
+  render() {
+    const { previousMessages } = this.state;
+    return (
+      <div className="app">
+      <ChatDisplayMessagesList previousMessages={previousMessages} /> 
+      <SendMessageContainer updateMessages={this.updateMessages} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/send-message/send-message-container.jsx
+++ src/components/send-message/send-message-container.jsx
@@ -8,6 +8,7 @@ class SendMessageContainer extends React.Component {
 
   sendMessage = (message) => {
     postMessage(message);
+    this.props.updateMessages();
   }
 
   handleChange = (e) => {
